1b2b276a506b8096d907d3885947d6ce342a8b77
vkalangu <vkalangu@in.ibm.com>
2017-08-10 16:12:59 +0530

    Create bindService2Cluster.tf

---
+++ bindService2Cluster.tf
@@ -0,0 +1,28 @@
+data "ibm_service_plan" "service_plan" {
+  service = "cloudantNoSQLDB"
+  plan    = "Lite"
+}
+
+resource "ibm_service_instance" "service" {
+  name       = "MyCloudantDB"
+  space_guid = "${data.ibm_space.spaceinst.id}"
+  service    = "${data.ibm_service_plan.service_plan.service}"
+  plan       = "${data.ibm_service_plan.service_plan.plan}"
+}
+
+resource "ibm_service_key" "serviceKey" {
+  name                  = "MyCloudantDBTestKey"
+  service_instance_guid = "${ibm_service_instance.service.id}"
+}
+
+
+resource "ibm_container_bind_service" "clusterbind" {
+  org_guid     = "${data.ibm_org.orginst.id}"
+  space_guid   = "${data.ibm_space.spaceinst.id}"
+  account_guid = "${data.ibm_account.acc.id}"
+  depends_on               = ["ibm_service_key.serviceKey"]
+  cluster_name_id          = "${ibm_container_cluster.testacc_cluster.name}"
+  service_instance_space_guid = "${data.ibm_space.spaceinst.id}"
+  service_instance_name_id = "${ibm_service_instance.service.id}"
+  namespace_id             = "default"
+}
